15b97ea06ca7a6b2229b3c989b232975474164cc
Ricardo-Vargas-Gonzalez <ceinnova05251@innovaccion.mx>
2023-08-25 10:31:11 -0600

    Version 1.0, ended

---
+++ Encriptador.js
@@ -0,0 +1,82 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".menssage");
+const copia = document.querySelector(".copias");
+copia.style.display = "none"
+
+
+function validarTexto(){
+    let textoEscrito = document.querySelector(".text-area").value;
+    let validador = textoEscrito.match(/^[a-z\s ñ ,]*$/);
+
+    if(!validador || validador === 0) {
+        alert("Solo son permitidas letras minúsculas y sin acentos")
+        location.reload();
+        return true;
+    }
+}
+
+
+function btnEncriptar(){
+    if(!validarTexto()) {
+        const textoEncriptado = encriptar(textArea.value)
+        mensaje.value = textoEncriptado
+        mensaje.style.backgroundImage = "none"
+        textArea.value = "";
+        copia.style.display = "block"
+    
+    }
+}
+
+/*Laves de encriptacion
+* la vocal a se codifica en "ai"`
+* la vocal e se codifica en "enter"`
+* la vocal i se codifica en "imes"`
+* la vocal o se codifica en "ober"`
+* la vocal o se codifica en "ufat"` */ 
+
+
+function encriptar(stringEncriptada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+            alert("Encriptación Exitosa")
+        }
+
+    }
+    return stringEncriptada
+}
+
+
+
+function btnDesencriptar(){
+    const textoEncriptado = desencriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+    
+}
+
+
+function desencriptar(stringDesencriptada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringDesencriptada.includes(matrizCodigo[i][1])){
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1] , matrizCodigo[i][0]);
+            alert("Texto Desencriptado")
+        }
+
+    }
+    return stringDesencriptada
+}
+
+
+function copiar(){
+    mensaje.select();
+    navigator.clipboard.writeText(mensaje.value)
+    mensaje.value = "";
+    alert("Texto Copiado")
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Encriptador de Texto
+
+Proyecto desarollado con conocimientos en html, css y javascript, obtenidos del programa, ONE (Oracle Next Education) de Alura Latama
+
+* versión 1.0
+* Developer: Ricardo Alonso Vargas González
